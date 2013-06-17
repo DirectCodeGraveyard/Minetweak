40ac3aa40eeffd8e6e6b34be96a551bbdbd6edad
Logan Gorence <loganjohngorence@gmail.com>
2013-06-17 06:00:44 -0700

    Initial Commit

---
+++ minecraft_server/com/directmyfile/betterthanbukkit/BetterThanBukkit.java
@@ -0,0 +1,13 @@
+package com.directmyfile.betterthanbukkit;
+
+import net.minecraft.server.MinecraftServer;
+
+public class BetterThanBukkit {
+
+    public static void main(String[] args) {
+        System.out.println("Success is very tasty.");
+
+        MinecraftServer.main(args);
+    }
+
+}
