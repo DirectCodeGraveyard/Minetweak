ed3e646de00989b31431fae32dc6314e841da0aa
Logan Gorence <loganjohngorence@gmail.com>
2013-06-17 23:38:17 -0700

    Added two kick player API methods, allowing you to either kick a player with the default kick message, or kick him with a custom kick message.

--- minecraft_server/com/directmyfile/betterthanbukkit/Server.java
+++ minecraft_server/com/directmyfile/betterthanbukkit/Server.java
@@ -2,12 +2,13 @@ package com.directmyfile.betterthanbukkit;
 
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.src.CommandBase;
+import net.minecraft.src.EntityPlayerMP;
 
 public class Server {
 
     /**
-     * Broadcast a message to the server
-     * @param message
+     * Broadcast a message to the server, this allows you to tell everyone something at the same time easily
+     * @param message The message you want to broadcast to every player and to the console
      * @return if the message was properly broadcasted
      */
     public static boolean broadcastMessage(String message) {
@@ -17,6 +18,37 @@ public class Server {
             return true;
         } else {
             return false;
+        }
+    }
+
+    /**
+     * Kick a player
+     * @param playerName Player's name
+     * @return true on successful kick
+     */
+    public static boolean kickPlayer(String playerName) {
+        EntityPlayerMP targetPlayer = MinecraftServer.getServer().getConfigurationManager().getPlayerEntity(playerName);
+        if (targetPlayer == null) {
+            return false;
+        } else {
+            targetPlayer.playerNetServerHandler.kickPlayer("You have been kicked from the server.");
+            return true;
+        }
+    }
+
+    /**
+     * Kick a player with a custom kick message
+     * @param playerName Player's name
+     * @param kickMessage Custom kick message
+     * @return true on successful kick
+     */
+    public static boolean kickPlayer(String playerName, String kickMessage) {
+        EntityPlayerMP targetPlayer = MinecraftServer.getServer().getConfigurationManager().getPlayerEntity(playerName);
+        if (targetPlayer == null) {
+            return false;
+        } else {
+            targetPlayer.playerNetServerHandler.kickPlayer("You have been kicked from the server.");
+            return true;
         }
     }
 
