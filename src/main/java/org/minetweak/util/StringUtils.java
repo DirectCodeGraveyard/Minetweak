3b15f73ae7fb59e2c7207c6360fc4e252d94424a
samrg472 <samrg472@gmail.com>
2013-07-04 20:52:32 -0400

    Exceptionless and fast way to check if a string is an integer

--- src/main/java/org/minetweak/util/StringUtils.java
+++ src/main/java/org/minetweak/util/StringUtils.java
@@ -22,10 +22,10 @@ public class StringUtils {
     }
 
     public static boolean isInteger(String s) {
-        try {
-            Integer.parseInt(s);
-        } catch(NumberFormatException e) {
-            return false;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!(c >= '0' && c <= '9'))
+                return false;
         }
         return true;
     }
