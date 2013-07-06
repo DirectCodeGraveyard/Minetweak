160780b11b82cf41a11138bd82dd160394129709
Logan Gorence <loganjohngorence@gmail.com>
2013-07-05 20:38:01 -0700

    Added PlayerDrinkPotionEvent, modified ItemPotion to trigger the event.

---
+++ src/main/java/org/minetweak/event/player/PlayerDrinkPotionEvent.java
@@ -0,0 +1,28 @@
+package org.minetweak.event.player;
+
+import net.minecraft.src.PotionEffect;
+import org.minetweak.entity.Player;
+
+/**
+ * Called when a player drinks a potion
+ * @see net.minecraft.src.ItemPotion:78
+ */
+public class PlayerDrinkPotionEvent {
+
+    private Player player;
+    private PotionEffect potionEffect;
+
+    public PlayerDrinkPotionEvent(Player player, PotionEffect potionEffect) {
+        this.player = player;
+        this.potionEffect = potionEffect;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public PotionEffect getPotionEffect() {
+        return potionEffect;
+    }
+
+}
