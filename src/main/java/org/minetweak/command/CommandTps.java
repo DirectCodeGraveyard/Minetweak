546bf0964de4d36e1dd4a1b58befb5cb9b9c9a7d
Kenneth Endfinger <kaendfinger@gmail.com>
2013-07-23 03:02:43 -0400

    Advanced Tab Completion and TPS command implemented. MINETWEAK-17 #close Command has been implemented.

---
+++ src/main/java/org/minetweak/command/CommandTps.java
@@ -0,0 +1,38 @@
+package org.minetweak.command;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.src.EnumChatFormatting;
+
+import java.text.DecimalFormat;
+
+public class CommandTps extends CommandExecutor {
+    private static final DecimalFormat	DF	= new DecimalFormat("########0.000");
+    @Override
+    public void executeCommand(CommandSender sender, String overallCommand, String[] args) {
+        String tps = getTPS(MinecraftServer.getServer().tickTimeArray);
+        if (tps.equals("20")) {
+            tps = EnumChatFormatting.GREEN + tps;
+        } else {
+            tps = EnumChatFormatting.YELLOW + tps;
+        }
+        sender.sendMessage(EnumChatFormatting.GOLD + "TPS" + EnumChatFormatting.RESET + ": " + tps);
+    }
+
+    public String getTPS(long[] tickArray) {
+        double tps = calcTicksInSecond(tickArray);
+        if (tps < 50)
+            return "20";
+        else
+            return DF.format(1000 / tps);
+    }
+
+    private double calcTicksInSecond(long[] tickArray) {
+        long var2 = 0L;
+
+        for (long var7 : tickArray) {
+            var2 += var7;
+        }
+
+        return (double) var2 / (double) tickArray.length * 1.0E-6D;
+    }
+}
