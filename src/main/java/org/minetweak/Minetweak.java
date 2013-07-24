33fcf1dd39a215a00428ccbafc241ff153ed7129
Logan Gorence <loganjohngorence@gmail.com>
2013-07-24 16:28:52 -0700

    Removed Minetweak join debug message

--- src/main/java/org/minetweak/Minetweak.java
+++ src/main/java/org/minetweak/Minetweak.java
@@ -167,7 +167,6 @@ public class Minetweak {
                 }
                 players.put(playerUsername, targetPlayerInstance);
             }
-            targetPlayerInstance.sendMessage("You were registered within Minetweak. Please check within the console for errors.");
             if (targetPlayerInstance.isOperator()) targetPlayerInstance.sendMessage("You are an op.");
             return true;
         }
