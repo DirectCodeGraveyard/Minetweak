b213f4f0b6a582d708d2c93ab0a3c0a0f10c2b3d
Logan Gorence <loganjohngorence@gmail.com>
2013-07-24 16:28:02 -0700

    Changed internal version number to 0.6.4

--- src/main/java/org/minetweak/Minetweak.java
+++ src/main/java/org/minetweak/Minetweak.java
@@ -34,7 +34,7 @@ public class Minetweak {
     /**
      * This is the Minetweak version we are currently running on.
      */
-    private static final String serverVersion = "0.6.3";
+    private static final String serverVersion = "0.6.4";
 
     /**
      * This boolean will return true if the server has finished loading, will
