82064a84a4d467d752d085770966d4307eb8b5b0
Logan Gorence <loganjohngorence@gmail.com>
2013-07-24 19:50:53 -0700

    Fixed Creative Sword bug

--- src/main/java/net/minecraft/item/ItemInWorldManager.java
+++ src/main/java/net/minecraft/item/ItemInWorldManager.java
@@ -238,6 +238,9 @@ public class ItemInWorldManager {
             if (!isCreative() && !getGameType().isAdventure() && !this.thisPlayerMP.canHarvestBlock(Block.blocksList[this.theWorld.getBlockId(par1, par2, par3)])) {
                 event.setCancelled(true);
             }
+            if (thisPlayerMP.getHeldItem().getItem() instanceof ItemSword) {
+                event.setCancelled(true);
+            }
             if (getGameType().isAdventure()) {
                 event.setCancelled(true);
             }
