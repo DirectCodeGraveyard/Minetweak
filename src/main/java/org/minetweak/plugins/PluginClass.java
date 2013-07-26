46576c00fe5b06217f2a86a951f46e4b3b50b811
Kenneth Endfinger <kaendfinger@gmail.com>
2013-07-26 00:05:24 -0400

    Added a @PluginClass annotation. Used for marking plugin classes in the IDE so they can be suppressed from warnings. Added a /slaughter command.

---
+++ src/main/java/org/minetweak/plugins/PluginClass.java
@@ -0,0 +1,8 @@
+package org.minetweak.plugins;
+
+/**
+ * Used to mark a plugin class to keep unused warning away in the IDE
+ * Very Useful for Plugins who are prone to checking for Warnings
+ */
+public @interface PluginClass {
+}
