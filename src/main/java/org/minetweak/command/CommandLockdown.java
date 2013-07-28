012e1e264b510eb3b95272d553b3f5e3ad23f644
Logan Gorence <loganjohngorence@gmail.com>
2013-07-27 23:34:21 -0700

    Added description comment for the entire CommandLockdown class.

--- src/main/java/org/minetweak/command/CommandLockdown.java
+++ src/main/java/org/minetweak/command/CommandLockdown.java
@@ -3,6 +3,11 @@ package org.minetweak.command;
 import org.minetweak.Minetweak;
 import org.minetweak.Server;
 
+/**
+ * Command that will set the server under lockdown and
+ * not allow anyone but ops in, but allow players online
+ * to stay on.
+ */
 public class CommandLockdown extends CommandExecutor {
     @Override
     public void executeCommand(CommandSender sender, String overallCommand, String[] args) {
