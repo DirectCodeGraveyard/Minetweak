6c56900f9cdda0d377721663cf6b8cb82866a67b
Logan Gorence <loganjohngorence@gmail.com>
2013-07-28 18:52:12 -0700

    Removed even more comments from Alex.

--- src/main/java/org/minetweak/event/block/BlockExpEvent.java
+++ src/main/java/org/minetweak/event/block/BlockExpEvent.java
@@ -2,9 +2,6 @@ package org.minetweak.event.block;
 
 import org.minetweak.block.IBlock;
 
-/**
- * An event that's called when a block yields experience.
- */
 public class BlockExpEvent extends BlockEvent {
     private int exp;
 
@@ -13,20 +10,10 @@ public class BlockExpEvent extends BlockEvent {
         this.exp = exp;
     }
 
-    /**
-     * Get the experience dropped by the block after the event has processed
-     *
-     * @return The experience to drop
-     */
     public int getExpToDrop() {
         return exp;
     }
 
-    /**
-     * Set the amount of experience dropped by the block after the event has processed
-     *
-     * @param exp 1 or higher to drop experience, else nothing will drop
-     */
     public void setExpToDrop(int exp) {
         this.exp = exp;
     }
