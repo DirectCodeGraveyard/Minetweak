884411c8af7ecff425329e3ea6f27074e7397738
Logan Gorence <loganjohngorence@gmail.com>
2013-07-28 18:50:06 -0700

    Removed more comments from Alex.

--- src/main/java/org/minetweak/event/block/BlockEvent.java
+++ src/main/java/org/minetweak/event/block/BlockEvent.java
@@ -2,9 +2,6 @@ package org.minetweak.event.block;
 
 import org.minetweak.block.IBlock;
 
-/**
- * Represents a block related event.
- */
 public abstract class BlockEvent {
     protected IBlock block;
 
@@ -12,11 +9,6 @@ public abstract class BlockEvent {
         block = theBlock;
     }
 
-    /**
-     * Gets the block involved in this event.
-     *
-     * @return The TweakBlock which block is involved in this event
-     */
     public final IBlock getBlock() {
         return block;
     }
