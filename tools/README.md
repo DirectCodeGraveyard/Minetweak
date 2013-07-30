0e831acec1638600f242577e4aec4e462421d786
Kenneth Endfinger <kaendfinger@gmail.com>
2013-07-30 08:14:19 -0400

    Removed build_json.py, and added a countClasses Groovy script.

---
+++ tools/README.md
@@ -0,0 +1,4 @@
+Minetweak Tools
+---------------
+
+Minetweak has some utilities to make development easier. You can find the tools in the 'tools/' folder of your Minetweak project.
---
+++ tools/countClasses.groovy
@@ -0,0 +1,9 @@
+def mainDirectory = new File(getClass().protectionDomain.codeSource.location.path).getParentFile().getParentFile()
+def sourceDirectory = new File(mainDirectory, 'src/main/java/org/minetweak')
+int classCount = 0
+sourceDirectory.eachFileRecurse {
+    if (it.getName().endsWith(".java")) {
+        classCount++
+    }
+}
+println(String.format('Found %s Minetweak classes.', classCount))
