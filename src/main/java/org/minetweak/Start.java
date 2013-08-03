8db96b408fb28b9187ce54dc764e7774e2a1aaac
Kenneth Endfinger <kaendfinger@gmail.com>
2013-08-02 20:12:26 -0400

    Fix all the bugs. I wonder whats going on......

---
+++ src/main/java/org/minetweak/Start.java
@@ -0,0 +1,10 @@
+package org.minetweak;
+
+/**
+ * Main Class for Minetweak. Used to load Dependencies in the future.
+ */
+public class Start {
+    public static void main(String[] args) {
+        Minetweak.main(args);
+    }
+}
