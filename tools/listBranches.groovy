9570abcc8e7f05e119b9ac0b6f1d2d14975280f1
Kenneth Endfinger <kaendfinger@gmail.com>
2013-08-30 23:53:42 -0400

    A bit of tweaking to listBranches.groovy

--- tools/listBranches.groovy
+++ tools/listBranches.groovy
@@ -1,7 +1,8 @@
 import groovy.json.JsonSlurper
 
 def url = 'https://api.github.com/repos/Minetweak/Minetweak/branches'
-def loadJSON = { String jsonURL -> return new JsonSlurper().parse(url.toURL().openStream().newReader()) as List<Map<String, String>> }
+def loadJSON = { String jsonURL -> return new JsonSlurper().parse(url.toURL().openStream().newReader()) 
+as List<Map<String, String>> }
 def branchInfo = loadJSON url
 def branches = []
 branchInfo.each { branches.add(it.get('name')) }
