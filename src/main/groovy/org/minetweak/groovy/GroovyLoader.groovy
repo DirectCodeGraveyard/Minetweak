a312caf1df3d7576b9622d8e9b20c72babe32ca8
Kenneth Endfinger <kaendfinger@gmail.com>
2013-10-05 15:30:32 -0400

    Added a more efficient Importer for Groovy Scripts using ImportCustomizer

--- src/main/groovy/org/minetweak/groovy/GroovyLoader.groovy
+++ src/main/groovy/org/minetweak/groovy/GroovyLoader.groovy
@@ -2,12 +2,14 @@ package org.minetweak.groovy
 
 import com.google.common.eventbus.Subscribe
 import groovy.io.FileType
+import org.codehaus.groovy.control.CompilerConfiguration
+import org.codehaus.groovy.control.customizers.ImportCustomizer
 import org.minetweak.Minetweak
 
 class GroovyLoader {
     static scriptDir = new File("./plugins/")
 
-    static def shell = new GroovyShell()
+    static def shell = new GroovyShell(prepare())
 
     static boolean loaded = {
         return load()
@@ -16,15 +18,18 @@ class GroovyLoader {
     static def load() {
         scriptDir.eachFileRecurse(FileType.FILES) {
             if (!it.name.endsWith(".groovy")) return
-            def script = shell.parse(prepare(it.text))
+            def script = shell.parse(it.text)
             Minetweak.registerListener(script)
             script.run()
         }
         return true
     }
 
-    static def prepare(String scriptText) {
-        def s = System.lineSeparator()
-        return "import ${Subscribe.class.name}${s}import ${Minetweak.class.name}${s}${scriptText}"
+    private static prepare() {
+        def config = new CompilerConfiguration()
+        def imports = new ImportCustomizer()
+        imports.addImports(Minetweak.class.name, Subscribe.class.name)
+        config.addCompilationCustomizers(imports)
+        return config
     }
 }
